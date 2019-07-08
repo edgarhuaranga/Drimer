fb6f30b46a4eb1b4c3918cc0f377186d1814be7c
Edgar Huaranga <edgar.huaranga@uni.pe>
2019-07-08 16:57:44 -0500

    index promotor

--- resources/views/promotor/index.blade.php
+++ resources/views/promotor/index.blade.php
@@ -48,7 +48,7 @@
                                             <label for="text-input-password">Tienda</label>
                                             <select class="form-control" id="tienda_id" name="tienda_id">
                                                 @foreach($tiendas as $tienda)
-                                                    <option value="{{$tienda->id}}">{{$tienda->cadena()."-".$tienda->nombre_sede->nombre}}</option>
+                                                    <option value="{{$tienda->id}}">{{$tienda->cadena()->nombre."-".$tienda->nombre_sede}}</option>
                                                 @endforeach
                                             </select>
                                         </div>
